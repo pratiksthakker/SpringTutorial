b06165d5a916abd02c8c3975442a381a76c907cc
sheetal <sheetal@192.168.1.2>
2018-10-15 22:53:47 +0200

    Adding comments as notes for the tutorial

--- springSelfLearning/springSelfLearning/src/main/java/self/learning/spring/App.java
+++ springSelfLearning/springSelfLearning/src/main/java/self/learning/spring/App.java
@@ -5,7 +5,7 @@ import spring.demo.first.Coach;
 import spring.demo.first.TrackCoach;
 
 /**
- * Hello world!
+ * Hello world!s
  *
  */
 public class App 
@@ -13,6 +13,10 @@ public class App
     public static void main( String[] args )
     {
     	//Coach coach = new BaseBallCoach(); 
+    	/*
+    	 * Here we see that even though Coach is a interface we have to define what kind of Object is actually initiaed (BaseBallCoach)
+    	 * So in order to avoid this (kind of hard coding) we will use Inversion of Control
+    	 */
     	Coach coach = new TrackCoach(); 
         System.out.println(coach.getDailyWorkOut());
     }
