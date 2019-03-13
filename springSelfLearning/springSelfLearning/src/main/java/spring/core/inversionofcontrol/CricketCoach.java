62cd8990e53759631a5a93cd429b1b631f8295fd
pratik thakker <pratiksthakker@gmail.com>
2019-03-13 22:00:40 +0200

    DI -- Setter Injection

--- springSelfLearning/springSelfLearning/src/main/java/spring/core/inversionofcontrol/CricketCoach.java
+++ springSelfLearning/springSelfLearning/src/main/java/spring/core/inversionofcontrol/CricketCoach.java
@@ -1,19 +1,28 @@
 package spring.core.inversionofcontrol;
 
+import spring.core.dependencyinjection.FortuneService;
+
 public class CricketCoach implements Coach {
+
+	FortuneService myFortuneService;//Here we will use Setter Dependency Injection to Inject this bean
+	
+	public FortuneService getMyFortuneService() {
+		return myFortuneService;
+	}
+
+	//wiil be used for Setter Injection , check the context xml file
+	public void setMyFortuneService(FortuneService myFortuneService) {
+		this.myFortuneService = myFortuneService;
+	}
 
 	@Override
 	public String getDailyWorkOut() {
-		
 		return "Spend atleast 45 mins on Fielding practice daily";
-				
-		
 	}
 
 	@Override
 	public String getDailyFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Cricket Coach Fortune -- "+ myFortuneService.getFortune();
 	}
 
 }
