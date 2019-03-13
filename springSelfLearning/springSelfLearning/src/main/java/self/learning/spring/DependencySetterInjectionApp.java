500df55b857a92c4088930595b2169dc13210a7d
pratik thakker <pratiksthakker@gmail.com>
2019-03-13 22:11:24 +0200

    new App Class for Setter Injection Demo

---
+++ springSelfLearning/springSelfLearning/src/main/java/self/learning/spring/DependencySetterInjectionApp.java
@@ -0,0 +1,27 @@
+package self.learning.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.core.inversionofcontrol.Coach;
+
+public class DependencySetterInjectionApp {
+	
+	public static void main(String args[])
+	{
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		Coach iocCoach = context.getBean("myCricketCoach", Coach.class); // what implementation would be picked up is configured in the applicationContext.xml
+
+		System.out.println(iocCoach.getDailyWorkOut());// IOC
+		
+		System.out.println(iocCoach.getDailyFortune());//DI
+		
+		
+		context.close(); 
+		
+		
+	}
+	
+
+}
